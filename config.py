da07246a079c458118d95a8dc6706e0b62c89f56
panwarsourabh339-coder <panwarsourabh339@gmail.com>
2025-09-24 23:51:07 +0530

    Update config.py
    Signed-off-by: panwarsourabh339-coder <panwarsourabh339@gmail.com>

--- config.py
+++ config.py
@@ -113,4 +113,4 @@ if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
-        )
+)
